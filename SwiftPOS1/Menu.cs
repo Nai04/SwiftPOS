610b69f3bf335e2a678777642fd6387cbd239d0b
IanMaloloy-on <ianmaloloy16@gmail.com>
2026-03-01 23:12:39 +0800

    Side panel, buttons, coffee and milktea Tab done

--- SwiftPOS1/Menu.cs
+++ SwiftPOS1/Menu.cs
@@ -23,5 +23,42 @@ namespace SwiftPOS1
             analytics.Show();  this.Hide();
 
         }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneButton7_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show(); this.Hide();
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            Coffee coffee = new Coffee();
+            coffee.Show();
+            this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        // Auto Resize form to fit the screen (hopefully it works)
+        private void Coffee_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnMilktea_Click(object sender, EventArgs e)
+        {
+            MilkTea milkTea = new MilkTea();
+            milkTea.Show();
+            this.Hide();
+        }
     }
+
+
 }
